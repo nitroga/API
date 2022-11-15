fb975a0ca03ebf01ab6f4e42add03350b64ba252
Alexander Sundvisson <nitrogaplays@gmail.com>
2022-11-15 13:33:04 +0100

    api ok

---
+++ API/Pokemon.cs
@@ -0,0 +1,6 @@
+public class Pokemon
+{
+    public string name { get; set; }
+    public int weight { get; set; }
+    public bool is_default { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+using System.Text.Json;
+using System.Net;
+
+RestClient pokeClient = new("https://pokeapi.co/api/v2/");
+RestRequest request = new("pokemon/ditto");
+RestResponse response = pokeClient.GetAsync(request).Result;
+
+Pokemon p = JsonSerializer.Deserialize<Pokemon>(response.Content);
+
+if (response.StatusCode == HttpStatusCode.OK) {
+    Console.WriteLine(p.name);
+    Console.WriteLine(p.weight);
+    Console.WriteLine(p.is_default);
+}
+
+else {
+    Console.WriteLine("Error: Pokemon probably not found");
+}
+
+Console.ReadLine();
